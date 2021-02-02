4cb1dc6fb8d08f63d900860d540b2ec38a260a54
TheCapn-MEC <erolda266@west-mec.org>
2021-02-01 22:16:03 -0700

    did some work

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var input1 = "";
 var input2 = "";
 var operation = "";
 
+/*
 function doMath(){
     if(operation != ""){
         var answer = 0;
@@ -99,4 +100,118 @@ function factorial(){
     }
     document.getElementById("answer").innerHTML = factorial;
     input1 = factorial;
+}
+*/
+
+var equation = "";
+var mockEquation = "";
+function pressButton(input){
+    switch(true){
+        case input == " % ":
+            mockEquation += " mod ";
+            break;
+        case input == "Math.PI":
+            mockEquation += "π";
+            break;
+        default:
+            mockEquation += input;
+    }
+    equation += input;
+    document.getElementById("answer").innerHTML = mockEquation;
+}
+function doMath(){
+    equation = eval(equation).toString();
+    mockEquation = equation;
+    document.getElementById("answer").innerHTML = mockEquation;
+}
+function clearCalc(){
+    equation = "";
+    mockEquation = '';
+    document.getElementById("answer").innerHTML = "0";
+}
+function deletion(){
+    if(equation.substring(equation.length-1) == " "){
+        equation = equation.substring(0,equation.length-3);
+        mockEquation = mockEquation.substring(0,mockEquation.length-3);
+    }else{
+        equation = equation.substring(0,equation.length-1);
+        mockEquation = mockEquation.substring(0,mockEquation.length-1);
+    }
+    document.getElementById("answer").innerHTML = mockEquation;
+}
+function square(){
+    var temp = "";
+    var arr = equation.split(' ');
+
+    arr[arr.length-1] = Math.pow(arr[arr.length-1],2).toString();
+    for(i = 0; i < arr.length; i++){
+        temp += arr[i].toString();
+    }
+    equation = temp;
+    mockEquation = temp;
+    document.getElementById("answer").innerHTML = mockEquation;
+}
+function sqrt(){
+    var temp = "";
+    var arr = equation.split(' ');
+
+    arr[arr.length-1] = Math.pow(arr[arr.length-1],0.5).toString();
+    for(i = 0; i < arr.length; i++){
+        temp += arr[i].toString();
+    }
+    equation = temp;
+    mockEquation = temp;
+    document.getElementById("answer").innerHTML = mockEquation;
+}
+function reciprocal(){
+    var temp = "";
+    var arr = equation.split(' ');
+
+    arr[arr.length-1] = (1 / arr[arr.length-1]).toString();
+    for(i = 0; i < arr.length; i++){
+        temp += arr[i].toString();
+    }
+    equation = temp;
+    mockEquation = temp;
+    document.getElementById("answer").innerHTML = mockEquation;
+}
+function absoluteVal(){
+    var temp = "";
+    var arr = equation.split(' ');
+
+    arr[arr.length-1] = (Math.abs(arr[arr.length-1])).toString();
+    for(i = 0; i < arr.length; i++){
+        temp += arr[i].toString();
+    }
+    equation = temp;
+    mockEquation = temp;
+    document.getElementById("answer").innerHTML = mockEquation;
+}
+function changeSign(){
+    var temp = "";
+    var arr = equation.split(' ');
+
+    arr[arr.length-1] = (arr[arr.length-1] * -1).toString();
+    for(i = 0; i < arr.length; i++){
+        temp += arr[i].toString();
+    }
+    equation = temp;
+    mockEquation = temp;
+    document.getElementById("answer").innerHTML = mockEquation;
+}
+function factorial(){
+    var temp = "";
+    var arr = equation.split(' ');
+    var factorial = 1;
+    for(i = 1; i <= arr[arr.length-1]; i++){
+        factorial *= i;
+    }
+    arr[arr.length-1] = factorial;
+
+    for(i = 0; i < arr.length; i++){
+        temp += arr[i].toString();
+    }
+    equation = temp;
+    mockEquation = temp;
+    document.getElementById("answer").innerHTML = mockEquation;
 }
